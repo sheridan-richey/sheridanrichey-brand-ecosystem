5718d7d325c8c29d6c528213d85bb786414b0834
Sheridan Richey <sheridan@sheridanrichey.com>
2025-08-05 22:52:24 -0600

    \Update Next.js config for static file serving and remove test page"

--- website/next.config.js
+++ website/next.config.js
@@ -3,6 +3,9 @@ const { withContentlayer } = require('next-contentlayer')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // App router is now default, no need for experimental.appDir
+  trailingSlash: false,
+  assetPrefix: '',
+  basePath: '',
 }
 
 module.exports = withContentlayer(nextConfig) 
